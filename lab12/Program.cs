fe0590518bfea8b07ae5ffebb878e5cf8e9b3e1e
Ilya Afanasyev <affeeal@yandex.ru>
2024-01-17 15:56:20 +0300

    add disconnected layer

--- lab12/Program.cs
+++ lab12/Program.cs
@@ -6,81 +6,144 @@ namespace Lab12
 {
   class Program
   {
-    static async Task Main(string[] args)
+    static void Main(string[] args)
     {
       string connectionString = ConfigurationManager.
           ConnectionStrings["DefaultConnection"].ConnectionString;
+      
+      // testConnectedLayer(connectionString);
+      // testDisconnectedLayer(connectionString);
+    }
 
-      // Связный уровень
+    static void testConnectedLayer(string connectionString)
+    {
       using (SqlConnection connection = new SqlConnection(connectionString))
       {
-        await connection.OpenAsync();
+        connection.Open();
 
         // Добавление данных таблицы
         {
           SqlCommand command = new SqlCommand(
               "INSERT INTO Users (Email, Name, PasswordHash) " +
-              "VALUES (@email, @name, @passwordHash)", connection);
+              "VALUES (@Email, @Name, @PasswordHash)", connection);
+          command.Parameters.Add(new SqlParameter("@Email", "user3@gmail.com"));
+          command.Parameters.Add(new SqlParameter("@Name", "user3"));
+          command.Parameters.Add(new SqlParameter("@PasswordHash", "hash"));
 
-          command.Parameters.Add(new SqlParameter("@email", "user3@gmail.com"));
-          command.Parameters.Add(new SqlParameter("@name", "user3"));
-          command.Parameters.Add(new SqlParameter("@passwordHash", "hash"));
-
-          int rowsAffected = await command.ExecuteNonQueryAsync();
-          Console.WriteLine($"Добавлено объектов: {rowsAffected}");
+          command.ExecuteNonQuery();
         }
 
         // Обновление данных таблицы
         {
           SqlCommand command = new SqlCommand(
-            "UPDATE Users SET BirthDate = @birthDate WHERE Email = @email",
+            "UPDATE Users SET BirthDate = @BirthDate WHERE Id = @Id",
             connection);
+          command.Parameters.Add(new SqlParameter("@BirthDate", "1990-01-01"));
+          command.Parameters.Add(new SqlParameter("@Id", 1));
 
-          command.Parameters.Add(new SqlParameter("@birthDate", "1990-01-01"));
-          command.Parameters.Add(new SqlParameter("@email", "user3@gmail.com"));
-
-          int rowsAffected = await command.ExecuteNonQueryAsync();
-          Console.WriteLine($"Обновлено объектов: {rowsAffected}");
+          command.ExecuteNonQuery();
         }
 
         // Удаление данных таблицы
         {
           SqlCommand command = new SqlCommand(
-            "DELETE Users WHERE Email = @email", connection);
+            "DELETE Users WHERE Email = @Email", connection);
+          command.Parameters.Add(new SqlParameter("@Email", "user2@gmail.com"));
 
-          command.Parameters.Add(new SqlParameter("@email", "user2@gmail.com"));
-
-          int rowsAffected = await command.ExecuteNonQueryAsync();
-          Console.WriteLine($"Удалено объектов: {rowsAffected}");
+          command.ExecuteNonQuery();
         }
 
         // Просмотр содержимого таблицы
         {
           SqlCommand command = new SqlCommand(
               "SELECT * FROM Users", connection);
-
-          using (SqlDataReader reader = await command.ExecuteReaderAsync())
+          using (SqlDataReader reader = command.ExecuteReader())
           {
             for (int i = 0; i < reader.FieldCount; i++)
-              if (i < reader.FieldCount - 1)
-                Console.Write($"{reader.GetName(i)}\t");
-              else
-                Console.WriteLine($"{reader.GetName(i)}");
+              Console.Write($"{reader.GetName(i)}\t");
+            Console.WriteLine();
 
-            while (await reader.ReadAsync())
+            while (reader.Read())
             {
               for (int i = 0; i < reader.FieldCount; i++)
-                if (i < reader.FieldCount - 1)
-                  Console.Write($"{reader.GetValue(i).ToString()}\t");
-                else
-                  Console.WriteLine($"{reader.GetValue(i).ToString()}");
+                Console.Write($"{reader.GetValue(i).ToString()}\t");
+              Console.WriteLine();
             }
           }
         }
       }
+    }
 
-      // Несвязный уровень
-      // ...
+    static void testDisconnectedLayer(string connectionString)
+    {
+      using (SqlConnection connection = new SqlConnection(connectionString))
+      {
+        using (SqlDataAdapter adapter = new SqlDataAdapter(
+            "SELECT * FROM Users", connection))
+        {
+          adapter.InsertCommand = new SqlCommand(
+              "INSERT Users (Email, Name, PasswordHash) " +
+              "VALUES (@Email, @Name, @PasswordHash)", connection);
+          adapter.InsertCommand.Parameters.Add(
+              "@Email", SqlDbType.NVarChar, 320, "Email");
+          adapter.InsertCommand.Parameters.Add(
+              "@Name", SqlDbType.NVarChar, 50, "Name");
+          adapter.InsertCommand.Parameters.Add(
+              "@PasswordHash", SqlDbType.NVarChar, 100, "PasswordHash");
+
+          adapter.UpdateCommand = new SqlCommand(
+              "UPDATE Users SET BirthDate = @BirthDate WHERE Id = @Id",
+              connection);
+          adapter.UpdateCommand.Parameters.Add(
+              "@BirthDate", SqlDbType.Date, 0, "BirthDate");
+          SqlParameter parameter = adapter.UpdateCommand.Parameters.Add(
+              "@Id", SqlDbType.Int, 0, "Id");
+          parameter.SourceVersion = DataRowVersion.Original;
+
+          adapter.DeleteCommand = new SqlCommand(
+              "DELETE Users WHERE Email = @Email", connection);
+          parameter = adapter.DeleteCommand.Parameters.Add(
+              "@Email", SqlDbType.NVarChar, 320, "Email");
+          parameter.SourceVersion = DataRowVersion.Original;
+
+          DataTable usersTable = new DataTable();
+          adapter.Fill(usersTable);
+
+          // Добавление данных
+          DataRow newRow = usersTable.NewRow();
+          newRow["Email"] = "user3@gmail.com";
+          newRow["Name"] = "user3";
+          newRow["PasswordHash"] = "hash";
+          usersTable.Rows.Add(newRow);
+
+          // Обновление данных
+          DataRow user1Row = usersTable.Rows[0];
+          user1Row["BirthDate"] = "1990-01-01";
+
+          // Удаление данных
+          usersTable.Rows[1].Delete();
+
+          adapter.Update(usersTable);
+
+          usersTable.Clear();
+          adapter.Fill(usersTable);
+
+          // Просмотр содержимого таблицы
+          using (DataTableReader reader = usersTable.CreateDataReader())
+          {
+            for (int i = 0; i < reader.FieldCount; i++)
+              Console.Write($"{reader.GetName(i)}\t");
+            Console.WriteLine();
+
+            while (reader.Read())
+            {
+              for (int i = 0; i < reader.FieldCount; i++)
+                Console.Write($"{reader.GetValue(i).ToString()}\t");
+              Console.WriteLine();
+            }
+          }
+        }
+      }
     }
   }
 }
